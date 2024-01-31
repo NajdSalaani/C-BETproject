aff6222d7f11f78ab4746807bc9a876b4543b024
NajdSalaani <116888807+NajdSalaani@users.noreply.github.com>
2024-02-01 00:18:24 +0100

    PULL/MERGE FROM DIADJI : UTILISATEUR.hpp

--- Utilisateur.hpp
+++ Utilisateur.hpp
@@ -1,22 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <string>
+<<<<<<< HEAD
+=======
+#include <list>
+>>>>>>> 2e92182 (m)
 #include "Pari.hpp"
 
 class Utilisateur{
     public:
     Utilisateur();
-    Utilisateur(const std::string &nom,const std::string& motDePasse);
+    Utilisateur(const std::string &nom);
     std::string getNom() const;
-    std::string getMotDePasse() const;
     void ajoutPari(const Pari& nouveauPari);
     void supprimerPari(const Pari& pariASupp);
+<<<<<<< HEAD
     void getMesParis() const;
+=======
+    std::string getMesParis() const;
+    std::list<Pari> getListParis() const;
+>>>>>>> 2e92182 (m)
 
 
     private:
     std::string _nom;
-    std::string _motDePasse;
+<<<<<<< HEAD
     std::vector<Pari> _mesParis;
-
+=======
+    std::list<Pari> _mesParis;
+>>>>>>> 2e92182 (m)
 };
