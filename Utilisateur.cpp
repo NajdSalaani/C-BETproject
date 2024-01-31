d9ea0ff5462af11192aabae20c17606dc66404f0
NajdSalaani <116888807+NajdSalaani@users.noreply.github.com>
2024-02-01 00:16:41 +0100

    Add files via upload

--- Utilisateur.cpp
+++ Utilisateur.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 #include <vector>
-<<<<<<< HEAD
-=======
-#include<sstream>
->>>>>>> 2e92182 (m)
 #include "Utilisateur.hpp"
 
 
@@ -13,13 +9,18 @@ using namespace std;
 Utilisateur::Utilisateur(){
 }
 
-Utilisateur::Utilisateur(const std::string& nom) : _nom(nom) {
-}
+/*Utilisateur::Utilisateur(const std::string& nom) : _nom(nom) {
+}*/
+
+Utilisateur::Utilisateur(const std::string& nom, const std::string &motDePasse) : _nom(nom), _motDePasse(motDePasse) {}
 
 std::string Utilisateur::getNom() const {
     return _nom;
 }
 
+std::string Utilisateur::getMotDePasse() const {
+    return _motDePasse;  //Pas opti niveau sécurité
+}
 
 void Utilisateur:: ajoutPari(const Pari& nouveauPari){
     _mesParis.push_back(nouveauPari);
@@ -27,7 +28,6 @@ void Utilisateur:: ajoutPari(const Pari& nouveauPari){
 
 
 
-<<<<<<< HEAD
 void Utilisateur:: getMesParis() const{
     std::cout << "Liste de mes paris :\n";
     for (const Pari& pari : _mesParis) {
@@ -43,30 +43,3 @@ void Utilisateur:: getMesParis() const{
 
 
 }
-=======
-std::string Utilisateur:: getMesParis() const{
-    std::ostringstream detailsStream;
-    detailsStream << "Liste de mes paris :\n";
-    for (const Pari& pari : _mesParis) {
-        if (pari.getStatut() == Statut::E){printf("bnojour");}
-        detailsStream << "Evenement : " << pari.getEvenement() << "\n";
-        detailsStream << "Cote : " << pari.getCote() << "\n";
-        detailsStream << "Montant mise : " << pari.getMontant() << "\n";
-        detailsStream << "Statut : " << (pari.getStatut() == Statut::E ? "En cours" : (pari.getStatut() == Statut::G ? "Gagne" : "Perdu")) << "\n";
-        detailsStream << "Choix : " << pari.getChoix() << "\n";
-        detailsStream << "-------------------------\n";
-        detailsStream << endl;
-    }
-    std::cout << endl;
-    return detailsStream.str();
-}
-
-
-std::list<Pari> Utilisateur:: getListParis() const{
-    std::list<Pari> listeParis;
-        for (const auto& paris : _mesParis) {
-            listeParis.push_back(paris);
-        }
-        return listeParis;
-    }
->>>>>>> 2e92182 (m)
